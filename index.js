e42920a35b0022b977c095b91e83b48f5b5af240
y-ohgi <ohgi.211@gmail.com>
2017-09-02 23:57:24 +0900

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { exec } = require('child_process')
 const ENV = process.env
 
-const TARGET_ROUTES = (ENV.TARGET_ROUTES || '久大本線,山陰本線').split(',')
+const TARGET_ROUTES = (ENV.TARGET_ROUTES || '山手線,中央線,東西線').split(',')
 
 exports.handler = (event, context, callback) => {
   fetchTrainDelayApi()
     .then(json => {
-      console.log("delay routes =====================")
+      console.log('delay routes =====================')
       console.log(json)
 
       let hitroutes = []
@@ -14,12 +14,11 @@ exports.handler = (event, context, callback) => {
         if( route.name.match(TARGET_ROUTES.join('|')) ) hitroutes.push(route.name)
       })
 
-
-      console.log("toot message =====================")
+      console.log('toot message =====================')
       const msg = buildTootMsg(hitroutes)
       console.log(msg)
       
-      toot(msg)
+      if(msg) toot(msg)
     })
     .then(stdout => callback(null, stdout))
     .catch(err => callback(err, null))
@@ -33,11 +32,9 @@ exports.handler = (event, context, callback) => {
  */
 const fetchTrainDelayApi = () => new Promise(
   (resolve, reject) => {
-    exec('curl https://rti-giken.jp/fhc/api/train_tetsudo/delay.json', (err, stdout, stderr) => {
-      if(err) reject(err)
-      
-      const json = JSON.parse(stdout)
-      resolve(json)
+    const cmd = 'curl https://rti-giken.jp/fhc/api/train_tetsudo/delay.json'
+    exec(cmd, (err, stdout, stderr) => {
+      err ? reject(err) : resolve(JSON.parse(stdout))
     })
   }
 )
@@ -48,7 +45,7 @@ const fetchTrainDelayApi = () => new Promise(
  * @param {Array} hitroutes 遅延中の路線名が格納された配列
  * @return {String}
  */
-const buildTootMsg = hitroutes => hitroutes.length ? `【遅延情報】\\n${hitroutes.join('\\n')}` : '【遅延情報】 \\n遅延している登録済みの路線はありません！'
+const buildTootMsg = hitroutes => hitroutes.length ? `【遅延情報】\\n${hitroutes.join('\\n')}` : ''
 
 /***
  * トゥートする
@@ -56,7 +53,7 @@ const buildTootMsg = hitroutes => hitroutes.length ? `【遅延情報】\\n${hit
  * @param {String} msg トゥートしたいテキスト
  * @return {String}
  */
-const toot = (msg) => new Promise(
+const toot = msg => new Promise(
   (resolve, reject) => {
     const cmd = `curl https://${ENV.MASTODON_HOST}/api/v1/statuses -X POST --header "Authorization: Bearer ${ENV.MASTODON_ACCESS_TOKEN}" --header "Content-Type: application/json" -d \'{"status":"${msg}"}\'`
     exec(cmd, (err, stdout, stderr) => {
