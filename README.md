f2b8c49efe82d024813b513f8bf523bcf1738c6d
y-ohgi <ohgi.211@gmail.com>
2017-09-02 23:20:19 +0900

    add README

---
+++ README.md
@@ -0,0 +1,34 @@
+train-delay-notify
+---
+
+# 概要
+マストドンに電車の遅延情報をお知らせ
+
+# Setup
+1. パッケージのインストール
+  - `$ npm install`
+2. 環境情報の編集
+  - `$ cp .env.example .env`
+  - `$ cp deploy.env.example deploy.env`
+3. デプロイ
+  - `$ npm run deploy`
+
+## 環境情報
+### .env
+* `AWS_ACCESS_KEY`
+  - AWS アクセスキー
+* `AWS_SECRET_ACCESS_KEY`
+  - AWS シークレットキー
+* `AWS_ROLE_ARN`
+  - Lambdaに紐付けるロール
+  - ex) `arn:aws:iam::000000000000000:role/AWSLambdaBasicExecutionRole`
+
+### deploy.env
+* `MASTODON_HOST`
+  - マストドンのホスト(ドメイン名)
+* `MASTODON_ACCESS_TOKEN`
+  - マストドンのアクセスキー
+* `TARGET_ROUTES`
+  - 遅延情報を取得したい路線
+  - csv形式で複数路線の登録が可能
+  - ex) `久大本線,山陰本線`
--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const { exec } = require('child_process')
 const ENV = process.env
 
+const TARGET_ROUTES = (ENV.TARGET_ROUTES || '久大本線,山陰本線').split(',')
+
 exports.handler = (event, context, callback) => {
-  toot("hoge~")
+  fetchTrainDelayApi()
+    .then(json => {
+      console.log("delay routes =====================")
+      console.log(json)
+
+      let hitroutes = []
+      json.forEach((route) => {
+        if( route.name.match(TARGET_ROUTES.join('|')) ) hitroutes.push(route.name)
+      })
+
+      console.log("toot message =====================")
+      const msg = buildTootMsg(hitroutes)
+      console.log(msg)
+      
+      toot(msg)
+    })
+    .then(stdout => callback(null, stdout))
+    .catch(err => callback(err, null))
 }
 
+/***
+ * 遅延情報の取得
+ *   MEMO: https://rti-giken.jp/fhc/api/train_tetsudo/
+ * 
+ * @return {Object} 遅延中の路線情報
+ */
+const fetchTrainDelayApi = () => new Promise(
+  (resolve, reject) => {
+    exec('curl https://rti-giken.jp/fhc/api/train_tetsudo/delay.json', (err, stdout, stderr) => {
+      if(err) reject(err)
+      
+      const json = JSON.parse(stdout)
+      resolve(json)
+    })
+  }
+)
 
+/***
+ * メッセージの組み立て
+ *
+ * @param {Array} hitroutes 遅延中の路線名が格納された配列
+ * @return {String}
+ */
+const buildTootMsg = hitroutes => `【遅延情報】\\n${hitroutes.join('\\n')}`
+
+/***
+ * トゥートする
+ * 
+ * @param {String} msg トゥートしたいテキスト
+ * @return {String}
+ */
 const toot = (msg) => new Promise(
   (resolve, reject) => {
     const cmd = `curl https://${ENV.MASTODON_HOST}/api/v1/statuses -X POST --header "Authorization: Bearer ${ENV.MASTODON_ACCESS_TOKEN}" --header "Content-Type: application/json" -d \'{"status":"${msg}"}\'`
